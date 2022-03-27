331821a13b56b5f48cc52b751d0ff559c2df6494
Mist <59188378+Mist0090@users.noreply.github.com>
2022-03-27 14:19:02 +0900

    Add files via upload

---
+++ dioxide5.0 pre - Peaceful/main-Dioxide/main.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using static DIOXIDE.Functions;
+using static DIOXIDE.execute;
+
+namespace DIOXIDE
+{
+    class main
+    {
+        static void Main(string[] cmd)
+        {
+            Application.EnableVisualStyles();
+
+            if (cmd.Length == 0)
+            {
+                executeP();
+            }
+            else
+            {
+                for (int i = 0; i < cmd.Length; i++)
+                {
+                    if (cmd[i] == "/P")
+                    {
+                        execute.executeP();
+                    }
+                    if (cmd[i] == "/EWP")
+                    {
+                        execute.executeEWP();
+                    }
+                    if (cmd[i] == "/help")
+                    {
+                        MessageBox.Show("/P Peaceful" + Environment.NewLine + "/EWP EnabledWarningPeaceful", "help", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+            }
+        }
+    }
+}
